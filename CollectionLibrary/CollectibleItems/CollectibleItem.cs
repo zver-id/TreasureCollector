5257a8c8abaa7abd9b18a98377d5480113ef44c0
zver-id <helix999@mail.ru>
2025-06-05 20:13:00 +0400

    Добавил конструктор в класс коллекционного предмета

--- CollectionLibrary/CollectibleItems/CollectibleItem.cs
+++ CollectionLibrary/CollectibleItems/CollectibleItem.cs
@@ -1,8 +1,14 @@
-﻿namespace CollectionLibrary;
+﻿namespace CollectionLibrary.CollectibleItems;
 
 public abstract class CollectibleItem
 {
-    public required CollectionItemType ItemType { get; init; }
+    public CollectionItemType ItemType { get; init; }
     public required string Name { get; set; }
     public Country? Country { get; set; }
+
+    protected CollectibleItem(CollectionItemType collectionItemType, Country? country = null)
+    {
+        this.ItemType = collectionItemType;
+        this.Country = country;
+    }
 }
