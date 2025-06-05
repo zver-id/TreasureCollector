82a3aafa7805497bd4c134a2ce98ad0e30b1ecd5
zver-id <helix999@mail.ru>
2025-06-05 20:13:31 +0400

    Добавил класс монеты

---
+++ CollectionLibrary/CollectibleItems/Coin.cs
@@ -0,0 +1,14 @@
+﻿namespace CollectionLibrary.CollectibleItems;
+
+public class Coin : CollectibleItem
+{
+    public string Nominal {get; set;}
+    public string Currency {get; set;}
+    public Coin(string nominal, string currency, CollectionItemType collectionItemType, Country? country):
+        base(collectionItemType, country)
+    {
+        this.Nominal = nominal;
+        this.Currency = currency;
+        this.Name = $"{nominal} {currency}";
+    }
+}
