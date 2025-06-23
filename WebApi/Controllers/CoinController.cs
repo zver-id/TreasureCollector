04619414e419bdbf8d10e5f9b7e646776b030a45
zver-id <helix999@mail.ru>
2025-06-23 22:39:15 +0400

    Переделал проект веб-api

---
+++ WebApi/Controllers/CoinController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CoinController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "SomeCoin";
+    }
+}
