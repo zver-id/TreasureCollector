316922f28ada5e2a6017bee282902c2bf20ba3f1
zver-id <helix999@mail.ru>
2025-06-25 06:38:54 +0400

    Добавлено фронтенд приложение

---
+++ FrontendApp/src/assets/components/screens/home/Home.jsx
@@ -0,0 +1,32 @@
+﻿import styles from './Home.module.css'
+import {coins} from "./coins.data.js";
+
+function Home() {
+
+    return (
+        <div>
+            <h1>Treasure Collector</h1>
+            <div>
+                {coins.map(coin => (
+                    <div key={coin.id} className={styles.item}>
+                        <div className={styles.image}
+                             style={{
+                                 backgroundImage: `url(${coin.image})`,
+                             }}
+                        ></div>
+                        <div className={styles.info}>
+                            <h2>{coin.name}</h2>
+                            <p>ID {coin.id}</p>
+                            <button>View</button>
+                        </div>
+                    </div>
+
+                )) }
+            </div>
+
+        </div>
+
+    )
+}
+
+export default Home
---
+++ FrontendApp/src/assets/components/screens/home/coins.data.js
@@ -0,0 +1,12 @@
+﻿export const coins = [
+    {
+        id: 1,
+        name: '1 dollar USD',
+        image: '/public/1.jpg'
+    },
+    {
+        id: 2,
+        name: '1 ruble Russia',
+        image: '/public/2.jpg'
+    }
+]
