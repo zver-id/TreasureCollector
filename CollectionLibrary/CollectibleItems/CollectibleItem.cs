c69e3bae2d6ffc25e095cb4ac4d9f161498a5eef
zver-id <helix999@mail.ru>
2025-07-17 22:37:15 +0400

    Добавил переопределение ToString в CollectibleItem

--- CollectionLibrary/CollectibleItems/CollectibleItem.cs
+++ CollectionLibrary/CollectibleItems/CollectibleItem.cs
@@ -1,11 +1,24 @@
-﻿namespace CollectionLibrary.CollectibleItems;
+﻿using CollectionLibrary.Nhibernate.Infrastructure;
+
+namespace CollectionLibrary.CollectibleItems;
 
 public abstract class CollectibleItem : IHasId
 {
   public virtual int Id { get; set; }
   public virtual CollectionItemType ItemType { get; init; }
+  
+  [Unique]
   public virtual string Name { get; set; }
   public virtual Country? Country { get; set; }
+
+  #region Базовый класс
+
+  public override string ToString()
+  {
+    return $"{this.ItemType.Name} with name {this.Name}";
+  }
+
+  #endregion
 
   protected CollectibleItem()
   {
