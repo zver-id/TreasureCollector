50b49f26a1fe05f0043bbf2a93e535346d691ee3
zver-id <helix999@mail.ru>
2025-07-26 11:35:23 +0400

    Пустые тесты для контроллеров

---
+++ WebApi.Tests/UnitTest1.cs
@@ -0,0 +1,30 @@
+using CollectionLibrary.CollectibleItems;
+using WebApi.ResponseContracts;
+
+namespace WebApi.Tests;
+
+public class Tests
+{
+  [SetUp]
+  public void Setup()
+  {
+  }
+
+  [Test]
+  public void MappingTest()
+  {
+    var coin = new Coin
+    {
+      Id = 1,
+      Country = new Country("Russia"),
+      Name = "1 rouble",
+      Nominal = "1",
+      Currency = "rouble",
+      ItemType = new CollectionItemType {Name = "Coin"},
+      
+    };
+    var response = new PartialCoinResponse(coin);
+    var expected = coin;
+    Assert.Pass();
+  }
+}
